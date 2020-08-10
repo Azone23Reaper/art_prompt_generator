c3613b68480d2ae467b4812d94e90e763a55e154
Elia Deppe <66836590+eliaCITS@users.noreply.github.com>
2020-08-10 16:50:39 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+# authors:
+# filename: 
+# date:
+#
+# description: <insert description here>
+#
+# modifications: <insert modifications made by whom and on what date>
+### imports ###
+    # insert imports here
+
+###############
+
+
+##### GLOBALS #####
+    # insert global variables here, explain what they are with comments
+    #   and include what type they are, and/or expected to be.
+    
+    
+###################
+    
+
+#################### FUNCTIONS ####################
+
+
+
+
+# name: main()
+# parameters
+#   none    # list each parameter by name, and explain what it is expected to hold
+# return values
+#   none    # list what value(s) a function returns, and why
+#
+# desc: <insert description of the function here>
+#
+# A note about main(). main() is typically a common function that is always called
+#   at the beginning of a program and is considered the function where the
+#   algorithm is created at the high-level (main points oriented)
+def main():
+    pass
+
+
+###################################################
+
+# Program Start
+if __name__ == '__main__': # a global variable, __name__, is set to the string '__main__' at the start of the program.
+    main()
