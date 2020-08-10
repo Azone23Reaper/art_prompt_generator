ae0603bc2c222bf3c3372f00e20419320f38368e
Elia Deppe <elia.deppe7@gmail.com>
2020-08-10 21:32:47 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,41 @@
 # authors:
-# filename: 
+# filename:
 # date:
 #
 # description: <insert description here>
 #
 # modifications: <insert modifications made by whom and on what date>
+
 ### imports ###
-    # insert imports here
+# insert imports here
 
 ###############
 
-
 ##### GLOBALS #####
-    # insert global variables here, explain what they are with comments
-    #   and include what type they are, and/or expected to be.
-    
-    
+# insert global variables here, explain what they are with comments
+#   and include what type they are, and/or expected to be. include who made them
+
 ###################
-    
 
 #################### FUNCTIONS ####################
 
 
 
 
+
+# !author: ALL
 # name: main()
 # parameters
 #   none    # list each parameter by name, and explain what it is expected to hold
 # return values
 #   none    # list what value(s) a function returns, and why
 #
-# desc: <insert description of the function here>
+# desc: <insert description of the function here. describe the function before
+#   creating.>
 #
 # A note about main(). main() is typically a common function that is always called
 #   at the beginning of a program and is considered the function where the
-#   algorithm is created at the high-level (main points oriented)
+#   algorithm is created at the high-level (main points/beats)
 def main():
     pass
 
@@ -42,5 +43,5 @@ def main():
 ###################################################
 
 # Program Start
-if __name__ == '__main__': # a global variable, __name__, is set to the string '__main__' at the start of the program.
+if __name__ == '__main__':  # a global variable, __name__, is set to the string '__main__' at the start of the program.
     main()
